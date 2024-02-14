fa02ff554bfb15116108d36666a21fd2ced820c3
Ragib-jahan <ragibjahan01@gmail.com>
2024-02-15 00:39:59 +0600

    Rectangle Cart added

---
+++ scripts/parallelogram-area.js
@@ -0,0 +1,15 @@
+function calculateParallelogramArea() {
+    // const baseInput = document.getElementById('parallelogram-base');
+    // const baseText = baseInput.value;
+    // const base = parseFloat(baseText);
+    // console.log(base);
+    const base = getParallelogramBase();
+    console.log(base);
+}
+
+function getParallelogramBase (){
+    const baseInput = document.getElementById('parallelogram-base');
+    const baseText = baseInput.value;
+    const base = parseFloat(baseText);
+    return base;
+}
