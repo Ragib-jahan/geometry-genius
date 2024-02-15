3bb9d6bb95b7b57f713027287e9d849db74a6c69
Ragib-jahan <ragibjahan01@gmail.com>
2024-02-15 09:38:48 +0600

    added the pentagon card

--- scripts/parallelogram-area.js
+++ scripts/parallelogram-area.js
@@ -1,15 +1,38 @@
+// function calculateParallelogramArea() {
+//     // const baseInput = document.getElementById('parallelogram-base');
+//     // const baseText = baseInput.value;
+//     // const base = parseFloat(baseText);
+//     // console.log(base);
+//     const base = getParallelogramBase();
+//     console.log(base);
+// }
+
+// function getParallelogramBase (){
+//     const baseInput = document.getElementById('parallelogram-base');
+//     const baseText = baseInput.value;
+//     const base = parseFloat(baseText);
+//     return base;
+// }
+
+
 function calculateParallelogramArea() {
-    // const baseInput = document.getElementById('parallelogram-base');
-    // const baseText = baseInput.value;
-    // const base = parseFloat(baseText);
-    // console.log(base);
-    const base = getParallelogramBase();
-    console.log(base);
+    const base = getInputValueById('parallelogram-base')
+    // console.log('base value', base);
+    const hight = getInputValueById('parallelogram-hight')
+    // console.log('Hight value', hight);
+    const area = base * hight;
+    console.log(area);
+    setInnerTextById('parallelogram-area', area);
+}
+function getInputValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    // console.log(inputValue);
+    return inputValue;
 }
 
-function getParallelogramBase (){
-    const baseInput = document.getElementById('parallelogram-base');
-    const baseText = baseInput.value;
-    const base = parseFloat(baseText);
-    return base;
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
